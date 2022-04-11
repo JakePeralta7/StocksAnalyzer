79e71b7f7fa2d64dc2354d6941336c763eefde45
Jake Peralta <58475124+JakePeralta7@users.noreply.github.com>
2022-04-11 16:48:02 +0300

    Add files via upload
    This my poc of what I can do with yfinance

---
+++ StockAnalyzer.py
@@ -0,0 +1,65 @@
+# Imports
+import yfinance as yf
+from datetime import datetime
+from os import path
+from queue import Queue
+
+
+class Stock:
+    def __init__(self, symbol):
+        self.data = yf.Ticker(symbol)
+        self.queue = Queue()
+    
+    def extract_info(self):
+        self.name = self.extract_value("longName")
+        self.symbol = self.extract_value("symbol")
+        self.business_summary = self.extract_value("longBusinessSummary")
+        self.industry = self.extract_value("industry")
+        self.sector = self.extract_value("sector")
+        self.country = self.extract_value("country")
+        self.current_price = self.extract_value("currentPrice")
+        self.market_cap = self.extract_value("marketCap")
+        self.volume = self.extract_value("volume")
+        self.average_volume = self.extract_value("averageVolume")
+        self.trailing_pe = self.extract_value("trailingPE")
+        self.forward_pe = self.extract_value("forwardPE")
+        self.beta = self.extract_value("beta")
+        self.dividend_yield = self.extract_value("yield")
+        self.queue.put(())
+    
+    def extract_value(self, key):
+        try:
+            return self.data.info[key]
+        except KeyError:
+            return None
+    
+    def analyze_earnings(self):
+
+        # Extracts stock's last four years earnings
+        self.earnings = self.data.get_earnings().to_dict()["Earnings"]
+        current_earnings = list(self.earnings.values())[-1]
+        for year, earnings in list(self.earnings.items())[:-1]:
+            if (earnings > 0):
+                growth = (current_earnings - earnings) / earnings
+            else:
+                growth = (current_earnings - earnings) / earnings * -1
+            print(f"Earning's growth since {year}: {growth:%}")
+    
+    def write_report(self):
+        
+        # Example for report name: aapl(11-04-22).txt
+        self.report_path = f"{self.symbol}({datetime.now().strftime('%d-%m-%y')}).txt"
+        
+        # Opens the file for editing
+        self.report = open(self.report_path, "w")
+
+
+def main():
+    stock_symbol = input("Enter stock symbol: ")
+    my_stock = Stock(stock_symbol)
+    my_stock.extract_info()
+    print(f"{my_stock.dividend_yield:.2%}")
+
+
+if __name__ == "__main__":
+    main()
