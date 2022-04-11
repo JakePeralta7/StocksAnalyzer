adfd1995ba5a8263c80c8dc608b46900def6d542
Jake Peralta <58475124+JakePeralta7@users.noreply.github.com>
2022-04-11 18:04:43 +0300

    Update StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -2,15 +2,18 @@
 import yfinance as yf
 from datetime import datetime
 from os import path
-from queue import Queue
+from docx import Document
 
 
 class Stock:
     def __init__(self, symbol):
         self.data = yf.Ticker(symbol)
-        self.queue = Queue()
     
     def extract_info(self):
+        """
+        Extracts intresting info about the stock
+        """
+
         self.name = self.extract_value("longName")
         self.symbol = self.extract_value("symbol")
         self.business_summary = self.extract_value("longBusinessSummary")
@@ -25,7 +28,11 @@ class Stock:
         self.forward_pe = self.extract_value("forwardPE")
         self.beta = self.extract_value("beta")
         self.dividend_yield = self.extract_value("yield")
-        self.queue.put(())
+        self.fifty_two_week_high = self.extract_value("fiftyTwoWeekHigh")
+        self.fifty_two_week_low = self.extract_value("fiftyTwoWeekLow")
+        self.two_hundred_day_average = self.extract_value("twoHundredDayAverage")
+        self.fifty_two_week_change = self.extract_value("52WeekChange")
+        self.s_and_p_fifty_two_week_change = self.extract_value("SandP52WeekChange")
     
     def extract_value(self, key):
         try:
@@ -55,7 +62,8 @@ class Stock:
 
 
 def main():
-    stock_symbol = input("Enter stock symbol: ")
+    #stock_symbol = input("Enter stock symbol: ")
+    stock_symbol = "kbwy"
     my_stock = Stock(stock_symbol)
     my_stock.extract_info()
     print(f"{my_stock.dividend_yield:.2%}")
